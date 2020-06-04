e432ec9a435013e00b547bc20311edb11155d236
Jasmine Aro <40184150+saira332@users.noreply.github.com>
2020-06-04 11:50:24 +0500

    api update

--- src/app/models/repository.ts
+++ src/app/models/repository.ts
@@ -1,7 +1,31 @@
 import { message } from "./message.model";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable() 
 export class Repository {
-    constructor() {         
-        this.message = JSON.parse(document.getElementById("data").textContent);
+    message: message; 
+    messageData: message;
+    constructor(private http: HttpClient)
+    {         
+        this.getmessage(2);
     }
-message: message; 
+    
+    getmessage(id: number)
+    {          
+        this.http.get<message>("/api/messages/" + id)
+        .subscribe(p => {
+            this.messageData = p;
+            console.log("Product Data Received");
+        });
+        // .subscribe(p => this.message = p);
+    }
+        get product(): message {
+            console.log("Product Data Requested"); 
+            return this.messageData;
+        }
+//     constructor() {         
+//         this.message = JSON.parse(document.getElementById("data").textContent);
+//     }
+
 }
